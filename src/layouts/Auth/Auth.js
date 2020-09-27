07e2e2b798040709d315177f8109b1581f551f30
Abizeyimana Victor <svicky.shema@gmail.com>
2020-09-27 11:05:51 +0200

    fix router

--- src/layouts/Auth/Auth.js
+++ src/layouts/Auth/Auth.js
@@ -1,17 +1,17 @@
 import React, { Fragment, Suspense } from 'react';
 import { renderRoutes } from 'react-router-config';
 import PropTypes from 'prop-types';
-import { LinearProgress } from '@material-ui/core';
+import { LinearProgress } from '@material-ui/core'
 
 import { Topbar, Footer } from './components';
 import { useStyles } from './styles';
-import { useLocation } from 'react-router-dom'
+import  useRouter  from 'utils/useRouter';
 
       
 const Auth = props => {
   const { route, t } = props;
 
-  const location = useLocation();
+  const {location} = useRouter();
   const classes = useStyles();
 
   return (
