ace8a7f9f56e504fe536c5e68a5934f3eb9f78ab
Akimana Jean d Amour <akimanaja17@gmail.com>
2021-03-23 07:40:45 +0200

    Add update and delete actions

--- src/redux/actions/slider.js
+++ src/redux/actions/slider.js
@@ -1,6 +1,11 @@
 import { store } from 'redux/store';
 import { http } from 'utils/http';
-import { CREATE_SLIDER, GET_SLIDERS } from './actionTypes';
+import {
+  CREATE_SLIDER,
+  GET_SLIDERS,
+  EDIT_SLIDER,
+  DELETE_SLIDER
+} from './actionTypes';
 
 export const createSlider = (newSlider = {}) => {
   store.dispatch({
@@ -12,5 +17,17 @@ export const getSliders = () => {
   store.dispatch({
     type: GET_SLIDERS,
     payload: http.get('/manage/sliders')
+  });
+};
+export const editSlider = (sliderBody = {}, sliderId) => {
+  store.dispatch({
+    type: EDIT_SLIDER,
+    payload: http.patch(`/manage/sliders/${sliderId}`, sliderBody)
+  });
+};
+export const deleteSlider = sliderId => {
+  store.dispatch({
+    type: DELETE_SLIDER,
+    payload: http.delete(`/manage/sliders/${sliderId}`)
   });
 };
